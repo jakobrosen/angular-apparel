84633bdcf1a47074e33626e15908177bf079ec7f
Jakob <jakobrosen0@gmail.com>
2026-09-03 18:35:02 +0200

    added sequelize config file

--- server/src/models/sequelize.ts
+++ server/src/models/sequelize.ts
@@ -3,14 +3,16 @@ import path from "path";
 
 // Absolute path so the server behaves the same regardless of which
 // directory it's launched from.
-const storage = path.join(import.meta.dirname, "..", "config", "database.db");
+const dbPath = path.join(import.meta.dirname, "..", "config", "database.db");
 
+// Sequelize database connection object.
 export const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage,
-  logging: false,
+  dialect: "sqlite", // Defines that we are using SQLITE.
+  storage: dbPath, // Path to the database file.
+  logging: false, // Turns off console logging of SQL queries.
   define: {
-    underscored: true,
-    timestamps: true,
+    // Defaults applied to every model defined using this connection.
+    underscored: true, // Translates camelCase to snake_case for the db.
+    timestamps: true, // Enables timestamps for each model
   },
 });
